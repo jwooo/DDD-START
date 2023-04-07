cda7203471acba6ae34422f88ac41506344ff8ae
jwooo <parkjwoo0515@gmail.com>
2023-04-07 21:13:22 +0900

    엔티티와 벨류

--- src/main/java/com/myshop/shop/order/command/Order.java
+++ src/main/java/com/myshop/shop/order/command/Order.java
@@ -21,7 +21,7 @@ import java.util.Objects;
  *
  */
 public class Order {
-    private OrderNo id;
+    private OrderNo number;
     private List<OrderLine> orderLines;
     private Orderer orderer;
     private ShippingInfo shippingInfo;
@@ -49,11 +49,15 @@ public class Order {
         calculateTotalAmounts();
     }
 
-    public void setShippingInfo(ShippingInfo shippingInfo) {
+    public void changeShippingInfo(ShippingInfo newShippingInfo) {
+        setShippingInfo(newShippingInfo);
+    }
+
+    private void setShippingInfo(ShippingInfo newShippingInfo) {
         if (shippingInfo == null) {
             throw new IllegalArgumentException("no ShippingInfo");
         }
-        this.shippingInfo = shippingInfo;
+        this.shippingInfo = newShippingInfo;
     }
 
     private void verifyAtLeastOneOrMoreOrderLines(List<OrderLine> orderLines) {
@@ -92,15 +96,15 @@ public class Order {
         if (o == null) return false;
         if (o.getClass() != Order.class) return false;
         Order other = (Order) o;
-        if (this.id == null) return false;
-        return this.id.equals(other.id);
+        if (this.number == null) return false;
+        return this.number.equals(other.number);
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((number == null) ? 0 : number.hashCode());
         return result;
     }
 
--- src/main/java/com/myshop/shop/order/command/Orderer.java
+++ src/main/java/com/myshop/shop/order/command/Orderer.java
@@ -1,4 +1,10 @@
 package com.myshop.shop.order.command;
 
+import lombok.Getter;
+
+@Getter
 public class Orderer {
+
+    private String name;
+    private String email;
 }
