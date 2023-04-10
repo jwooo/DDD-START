f0c5ca6a544400278b5c85b31b7002878ae78332
jwooo <parkjwoo0515@gmail.com>
2023-04-10 21:32:39 +0900

    엔티티와 밸류 기본 매핑 구현

--- src/main/java/com/myshop/shop/order/command/domain/ShippingInfo.java
+++ src/main/java/com/myshop/shop/order/command/domain/ShippingInfo.java
@@ -2,11 +2,30 @@ package com.myshop.shop.order.command.domain;
 
 import lombok.Getter;
 
+import javax.persistence.*;
+
+@Embeddable
 @Getter
 public class ShippingInfo {
-    private String receiverName;
-    private String receiverPhoneNumber;
-    private String shippingAddress1;
-    private String shippingAddress2;
-    private String shippingZipCode;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "zipCode", column = @Column(name = "shipping_zip_code")),
+            @AttributeOverride(name = "address1", column = @Column(name = "shipping_addr1")),
+            @AttributeOverride(name = "address2", column = @Column(name = "shipping_addr2"))
+    })
+    private Address address;
+
+    private String message;
+
+    @Embedded
+    private Receiver receiver;
+
+    protected ShippingInfo() {}
+
+    public ShippingInfo(Address address, String message, Receiver receiver) {
+        this.address = address;
+        this.message = message;
+        this.receiver = receiver;
+    }
 }
