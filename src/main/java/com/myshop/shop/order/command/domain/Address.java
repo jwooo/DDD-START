18e93f3aded121c7929360ad56553a581620fd67
jwooo <parkjwoo0515@gmail.com>
2023-04-22 20:09:46 +0900

    Address Create

--- src/main/java/com/myshop/shop/order/command/domain/Address.java
+++ src/main/java/com/myshop/shop/order/command/domain/Address.java
@@ -2,12 +2,23 @@ package com.myshop.shop.order.command.domain;
 
 import lombok.Getter;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 @Getter
 public class Address {
 
+    @Column(name = "address1")
     private String address1;
+
+    @Column(name = "address2")
     private String address2;
+
+    @Column(name = "zip_code")
     private String zipcode;
+
+    protected Address() {}
 
     public Address(String address1, String address2, String zipcode) {
         this.address1 = address1;
