6ad8f54e5f37739564d61a552f28b4737c832419
jwooo <parkjwoo0515@gmail.com>
2023-04-24 20:15:27 +0900

    OrderRepository Create

--- src/main/java/com/myshop/shop/order/command/domain/OrderRepository.java
+++ src/main/java/com/myshop/shop/order/command/domain/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.myshop.shop.order.command.domain;
 
-public interface OrderRepository {
-    Order findById(OrderNo no);
+import org.springframework.data.repository.Repository;
+
+import java.util.Optional;
+
+public interface OrderRepository extends Repository<Order, OrderNo> {
+    Optional<Order> findById(OrderNo id);
     void save(Order order);
     void delete(Order order);
 }
