87447c7f9ef3f91e0b47e36ef3d05d1903629499
jwooo <parkjwoo0515@gmail.com>
2023-04-28 21:22:41 +0900

    밸류 컬렉션을 @Entity로 매핑하기

--- src/main/java/com/myshop/shop/catalog/command/domain/product/Product.java
+++ src/main/java/com/myshop/shop/catalog/command/domain/product/Product.java
@@ -1,8 +1,11 @@
 package com.myshop.shop.catalog.command.domain.product;
 
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.myshop.shop.common.jpa.MoneyConverter;
+import com.myshop.shop.common.model.Money;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "product")
@@ -11,5 +14,25 @@ public class Product {
     @EmbeddedId
     private ProductId id;
 
+    private String name;
+
+    @Convert(converter = MoneyConverter.class)
+    private Money money;
+
+    private String detail;
+
+    @OneToMany(
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+            orphanRemoval = true
+    )
+    @JoinColumn(name = "product_id")
+    @OrderColumn(name = "list_idx")
+    private List<Image> images = new ArrayList<>();
+
     protected Product() {}
+
+    public void changeImages(List<Image> newImages) {
+        images.clear();
+        images.addAll(newImages);
+    }
 }
