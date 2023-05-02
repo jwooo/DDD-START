1ba60c2afa36c1ba41c96e61fd9c41c8cc0479bd
jwooo <parkjwoo0515@gmail.com>
2023-05-02 20:40:07 +0900

    Member Create

---
+++ src/main/java/com/myshop/shop/member/command/domain/Member.java
@@ -0,0 +1,64 @@
+package com.myshop.shop.member.command.domain;
+
+import com.myshop.shop.common.jpa.EmailSetConverter;
+import com.myshop.shop.common.model.Email;
+import com.myshop.shop.common.model.EmailSet;
+import com.myshop.shop.member.MemberId;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "member")
+public class Member {
+
+    @EmbeddedId
+    private MemberId id;
+
+    private String name;
+
+    @Embedded
+    private Password password;
+
+    private boolean blocked;
+
+    @Column(name = "emails")
+    @Convert(converter = EmailSetConverter.class)
+    private EmailSet emails;
+
+    protected Member() {}
+
+    public Member(MemberId id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public MemberId getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void changeEmails(Set<Email> emails) {
+        this.emails = new EmailSet(emails);
+    }
+
+    public void block() {
+        this.blocked = true;
+    }
+
+    public void unblock() {
+        this.blocked = false;
+    }
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public EmailSet getEmails() {
+        return emails;
+    }
+
+}
